502f615096563329928e2c5b1774e9d7887628d8
shefalii <singhshefali.1992@gmail.com>
2020-10-12 22:35:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./model/shortUrl');
+
+mongoose.connect('mongodb://localhost:27017/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', async (req, res) => {
+    const urls = await ShortUrl.find();
+    res.render('view', { urls: urls });
+})
+
+app.post('/shortUrlGenerator', async (req, res) => {
+    await ShortUrl.create({fullUrl: req.body.fullUrl});
+    res.redirect('/');
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const url = await ShortUrl.findOne({shortUrl: req.params.shortUrl});
+  if(url == null) return res.sendStatus(404);
+  url.save();
+  res.redirect(url.fullUrl);
+})
+app.listen(process.env.port || 5000);
