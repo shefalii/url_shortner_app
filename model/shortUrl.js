79131cc50655d2833b537451fcfb3472070585b4
shefalii <singhshefali.1992@gmail.com>
2020-10-12 22:37:00 +0530

    Add files via upload

---
+++ model/shortUrl.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+
+const shortUrlSchema = new mongoose.Schema({
+    fullUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        default: shortId.generate
+    }
+})
+
+module.exports = mongoose.model('urlShortener', shortUrlSchema)
